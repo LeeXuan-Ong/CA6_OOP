3b198db304eb7f570cc4147be29946cccc2c824e
Lee Xuan Ong <ongtan597@gmail.com>
2023-03-31 00:10:27 +0800

    update variable name

--- src/main/java/com/dkit/LeeXuanOng/SD2A/DTOs/Instrument.java
+++ src/main/java/com/dkit/LeeXuanOng/SD2A/DTOs/Instrument.java
@@ -1,11 +1,13 @@
 package com.dkit.LeeXuanOng.SD2A.DTOs;
 
+import java.util.Objects;
+
 public class Instrument {
 
     private int id;
     private String insName;
     private String insDesc;
-    private int insStrock;
+    private int insStock;
     private double insPrice;
     private String insCategory;
 
@@ -13,7 +15,7 @@ public class Instrument {
         this.id = id;
         this.insName = insName;
         this.insDesc = insDesc;
-        this.insStrock = insStrock;
+        this.insStock = insStrock;
         this.insPrice = insPrice;
         this.insCategory = insCategory;
     }
@@ -22,7 +24,7 @@ public class Instrument {
         //id need to be a new id that get from the mysql db;
         this.insName = insName;
         this.insDesc = insDesc;
-        this.insStrock = insStrock;
+        this.insStock = insStrock;
         this.insPrice = insPrice;
         this.insCategory = insCategory;
     }
@@ -51,12 +53,12 @@ public class Instrument {
         this.insDesc = insDesc;
     }
 
-    public int getInsStrock() {
-        return insStrock;
+    public int getInsStock() {
+        return insStock;
     }
 
-    public void setInsStrock(int insStrock) {
-        this.insStrock = insStrock;
+    public void setInsStock(int insStock) {
+        this.insStock = insStock;
     }
 
     public double getInsPrice() {
@@ -73,5 +75,29 @@ public class Instrument {
 
     public void setInsCategory(String insCategory) {
         this.insCategory = insCategory;
+    }
+
+    @Override
+    public String toString() {
+        return '\n'+"Instrument{" +
+                "id=" + id +
+                ", insName='" + insName + '\'' +
+                ", insDesc='" + insDesc + '\'' +
+                ", insStrock=" + insStock +
+                ", insPrice=" + insPrice +
+                ", insCategory='" + insCategory + '\'' +
+                '}' ;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Instrument that)) return false;
+        return id == that.id || (insStock == that.insStock && Double.compare(that.insPrice, insPrice) == 0 && insName.equals(that.insName) && Objects.equals(insDesc, that.insDesc) && Objects.equals(insCategory, that.insCategory));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, insName, insDesc, insStock, insPrice, insCategory);
     }
 }
