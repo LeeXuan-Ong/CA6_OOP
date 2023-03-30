3e1fc504377c14dfa1e32f1abaffceb2d05ae73e
Lee Xuan Ong <ongtan597@gmail.com>
2023-03-30 20:27:53 +0800

    for unit testing of features 1 to 4 but not 100% coverage yet due to catch

--- src/test/java/com/dkit/LeeXuanOng/SD2A/DAOs/InstrumentDAO/MySqlInstrumentDAOTest.java
+++ src/test/java/com/dkit/LeeXuanOng/SD2A/DAOs/InstrumentDAO/MySqlInstrumentDAOTest.java
@@ -1,4 +1,44 @@
+package com.dkit.LeeXuanOng.SD2A.DAOs.InstrumentDAO;
+
+import com.dkit.LeeXuanOng.SD2A.DAOException.DAOException;
+import com.dkit.LeeXuanOng.SD2A.DTOs.Instrument;
+import junit.framework.Assert;
 import junit.framework.TestCase;
-public class MySqlInstrumentDAOTest extends TestCase {
-  
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+public class MySqlInstrumentDAOTest {
+
+    @Test
+    public void testFindAllInstruments() throws DAOException {
+        MySqlInstrumentDAO m = new MySqlInstrumentDAO()   ;
+        List<Instrument> l = m.findAllInstruments();
+        boolean expected = l.size()>0;
+        assertTrue(expected);
+
+    }
+
+    @Test
+    public void testFindInstrumentByInstrumentId() throws DAOException{
+        MySqlInstrumentDAO m = new MySqlInstrumentDAO();
+        Instrument i = m.findInstrumentByInstrumentId(1);
+        assertEquals("Guitar", i.getInsName());
+    }
+
+    @Test
+    public void testAddInstrument() throws DAOException {
+        MySqlInstrumentDAO m = new MySqlInstrumentDAO();
+        int result = m.addInstrument(new Instrument( "Guitar", "Is a Guitar", 10, 100, "String"));
+        assert(result != -1);
+    }
+
+    @Test
+    public void testDeleteInstrument() throws DAOException {
+        MySqlInstrumentDAO m = new MySqlInstrumentDAO();
+        boolean result = m.deleteInstrument(30);
+        assertTrue(result);
+    }
+
 }
